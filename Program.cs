803fa504bebae7ca51653b95797dfd0623794ed7
TopRamen789 <TopRamen789@gmail.com>
2021-10-17 02:32:11 -0600

    Make some branches in test repo, add data.json to mock ticket data from 3rd party system, map tickets to branch names.

--- Program.cs
+++ Program.cs
@@ -1,25 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
 using LibGit2Sharp;
+using Newtonsoft.Json;
 
 namespace GitTools
 {
-    class Program
-    {
-        static void cmd_DataReceived(object sender, DataReceivedEventArgs e)
-        {
-            // Console.WriteLine("Output from other process");
-            Console.WriteLine(e.Data);
+    public class Ticket {
+        [JsonProperty("Ticket")]
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string StoryPoints { get; set; }
+    }
+
+    static class TicketQueryHelper {
+        public static Func<Ticket, bool> GetBugTickets = ticket => ticket.Name.Contains("bug");
+        public static Func<Ticket, bool> GetFeatureTickets = ticket => ticket.Name.Contains("task");
+    }
+
+    class Program {
+        static string GetDataFromFile() {
+            string fileData = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\data.json");
+            return fileData;
         }
 
+        static List<Ticket> GetTicketData() {
+            string fileData = GetDataFromFile();
+            return JsonConvert.DeserializeObject<List<Ticket>>(fileData);
+        }
 
-        static void Main(string[] args)
-        {
-            string repo = "";
-            var test = new Repository(repo);
-            foreach(var branch in test.Branches) {
-                // branch.RemoteName;
-            }
+        static Repository GetRepository() {
+            return new Repository($"{Directory.GetCurrentDirectory()}\\GitRepoTest");
+        }
+
+        static ILookup<string, IEnumerable<string>> MapBranches(Repository repository, List<Ticket> tickets) {
+            var ticketNames = tickets.Select(t => t.Name);
+            return repository.Branches.ToLookup(b => b.FriendlyName, b => ticketNames.Where(t => b.FriendlyName.Contains(t)));
+        }
+
+        static void Main(string[] args) {
+            List<Ticket> tickets = GetTicketData();
+            List<Ticket> bugTickets = tickets
+                .Where(ticket => TicketQueryHelper.GetBugTickets(ticket))
+                .ToList();
+            List<Ticket> featureTickets = tickets
+                .Where(ticket => TicketQueryHelper.GetFeatureTickets(ticket))
+                .ToList();
+            var test = GetRepository();
+            var mappedBranches = MapBranches(test, tickets);
         }
     }
 }
