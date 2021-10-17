9e15c73b60d77133faeb7457270adf7d4bca0f36
TopRamen789 <TopRamen789@gmail.com>
2021-10-16 19:40:35 -0600

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using LibGit2Sharp;
+
+namespace GitTools
+{
+    class Program
+    {
+        static void cmd_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            // Console.WriteLine("Output from other process");
+            Console.WriteLine(e.Data);
+        }
+
+
+        static void Main(string[] args)
+        {
+            string repo = "";
+            var test = new Repository(repo);
+            foreach(var branch in test.Branches) {
+                // branch.RemoteName;
+            }
+        }
+    }
+}
